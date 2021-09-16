9e6add415280556306919881ef3c361e2e38938f
Daniel Soto <dasg.1993@gmail.com>
2021-09-15 19:27:44 -0500

    [feature] - async await

---
+++ index.js
@@ -0,0 +1,41 @@
+// async/await
+
+// function f() {
+//    let promise = new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//          resolve('done');
+//       }, 1000);
+//    });
+
+//    let result = await promise;
+
+//    console.log(result);
+// }
+
+
+// f();
+
+async function showAvatar() {
+    // read our JSON
+    let response = await fetch('./data.json');
+    let user = await response.json();
+    console.log(user)
+    
+    // read github user
+
+    let githubResponse = await fetch(`https://api.github.com/users/${user.data[0].username}`);
+    let githubUser = await githubResponse.json();
+
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.classList.add('avatar');
+    document.body.appendChild(img);
+
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+    img.remove();
+
+    return githubUser;
+}
+
+showAvatar();
